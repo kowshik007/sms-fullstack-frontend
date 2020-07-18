f26b2ec67d51fd3f5cbe264c5dd20816981c8df6
kowshik007 <kowshikkondapaneni@gmail.com>
2020-07-18 22:31:15 +0530

    07/18/2020 [Kowshik] ReadMe.docx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -44,11 +44,7 @@ const routes: Routes = [
       {
         path: '',
         component: SearchFormComponent
-      },
-      {
-        path: 'results',
-        component: SearchResultsComponent
-      },
+      }
     ]
   }
 ];
